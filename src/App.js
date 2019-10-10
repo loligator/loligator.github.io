844267ee4d598a8926761d8ae715069201f0890c
Connor McDermott <connor.mcdermott@perficient.com>
2019-10-10 14:25:58 -0500

    Got bootstrap and reactstrap, created Navbar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Navbar,
+        Nav,
+        NavItem,
+        NavLink,
+        UncontrolledDropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem
+        } from 'reactstrap';
+
 function App() {
   return (
     <div className="App">
+      <Navbar color="dark" className="navbar-dark" dark expand="md">
+        <Nav className="mx-auto" navbar>
+          <NavItem>
+            <NavLink href="/">HOME</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              GAMES
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                MADE IN THE SHADE
+              </DropdownItem>
+              <DropdownItem>
+                CIRCUIT
+              </DropdownItem>
+              <DropdownItem>
+                SYNTHESIS
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+          <NavItem>
+            <NavLink href="/">CONTACT</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
